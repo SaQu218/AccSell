b9922feb192357031e638012b02e5d33199b24a7
SaQu218 <saqupriv@gmail.com>
2025-03-13 18:00:18 +0100

    Poprawki do server.js i struktury plików

--- public/login.js
+++ public/login.js
@@ -21,7 +21,7 @@ async function loginUser(event) {
         console.log(result);  // Sprawdzamy odpowiedź
 
         if (response.ok) {
-            // Zapisujemy token JWT w ciasteczkach
+            // Zapisujemy token JWT w ciasteczkach (jeśli chcesz to zrobić)
             document.cookie = `token=${result.token}; path=/; secure; HttpOnly;`;
 
             // Przekierowanie na stronę powitalną
